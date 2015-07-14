96e38003391a229d3138b9f7a450d6f6c0b89254
Zēng Liàng <ObjcZL@gmail.com>
2015-07-14 18:40:09 +0800

    Clean code style

--- Classes/MSFAuthorizeViewModel.m
+++ Classes/MSFAuthorizeViewModel.m
@@ -30,212 +30,204 @@ static const int kCounterLength = 60;
 #pragma mark - Lifecycle
 
 - (instancetype)initWithServer:(MSFServer *)server {
-  self = [super init];
-  if (!self) {
-    return nil;
-  }
-  _server = server;
-  _username = @"";
-  _password = @"";
-  _captcha = @"";
-  _counter = @"获取验证码";
-  _agreeOnLicense = NO;
-  _counting = NO;
-  
-  @weakify(self)
-  _executeSignIn = [[RACCommand alloc] initWithEnabled:self.signInValidSignal
-    signalBlock:^RACSignal *(id input) {
-      @strongify(self)
-      if (![self.username isMobile]) {
-        return [RACSignal error:[self.class errorWithFailureReason:@"请输入正确的手机号"]];
-      }
-      if (![self.password isPassword]) {
-        return [RACSignal error:[self.class errorWithFailureReason:@"请输入8位以上数字和字母混合密码"]];
-      }
-     
-      return [self executeSignInSignal];
-    }];
-  
-  _executeSignUp = [[RACCommand alloc] initWithEnabled:self.signUpValidSignal
-    signalBlock:^RACSignal *(id input) {
-      @strongify(self)
-      if (![self.username isMobile]) {
-        return [RACSignal error:[self.class errorWithFailureReason:@"请输入正确的手机号"]];
-      }
-      if (![self.captcha isCaptcha]) {
-        return [RACSignal error:[self.class errorWithFailureReason:@"请输入6位验证码"]];
-      }
-      if (![self.password isPassword]) {
-        return [RACSignal error:[self.class errorWithFailureReason:@"请输入8位以上数字和字母混合密码"]];
-      }
-      if (!self.agreeOnLicense) {
-        return [RACSignal error:[self.class errorWithFailureReason:@"请仔细阅读贷款协议"]];
-      }
-     
-      return [self executeSignUpSignal];
-    }];
-  
-  _executeAgreeOnLicense = [[RACCommand alloc] initWithSignalBlock:^RACSignal *(id input) {
-    @strongify(self)
-    self.agreeOnLicense = !self.agreeOnLicense;
-    
-    return [RACSignal return:@(self.agreeOnLicense)];
-  }];
-  
-  _executeCaptcha = [[RACCommand alloc] initWithEnabled:self.captchaRequestValidSignal signalBlock:^RACSignal *(id input) {
-    @strongify(self)
-    if (![self.username isMobile]) {
-      return [RACSignal error:[self.class errorWithFailureReason:@"请输入正确的手机号"]];
-    }
-    return [[self executeCaptchaSignal]
-      doNext:^(id x) {
-        @strongify(self)
-        self.counting = YES;
-        RACSignal *repetitiveEventSignal = [[RACSignal interval:1 onScheduler:RACScheduler.mainThreadScheduler] take:kCounterLength];
-        __block int repetCount = kCounterLength;
-        [repetitiveEventSignal subscribeNext:^(id x) {
-          self.counter = [@(--repetCount) stringValue];
-        } completed:^{
-          self.counter = @"获取验证码";
-          self.counting = NO;
-        }];
-      }];
-    }];
-  
-  _executeFindPassword = [[RACCommand alloc] initWithEnabled:self.findPasswordValidSignal
-    signalBlock:^RACSignal *(id input) {
-      @strongify(self)
-      if (![self.username isMobile]) {
-        return [RACSignal error:[self.class errorWithFailureReason:@"请输入正确的手机号"]];
-      }
-      if (![self.captcha isCaptcha]) {
-        return [RACSignal error:[self.class errorWithFailureReason:@"请输入6位验证码"]];
-      }
-      if (![self.password isPassword]) {
-        return [RACSignal error:[self.class errorWithFailureReason:@"请输入8位以上数字和字母混合密码"]];
-      }
-     
-      return [self executeFindPasswordSignal];
-    }];
-  
-  _executeFindPasswordCaptcha = [[RACCommand alloc] initWithEnabled:self.captchaRequestValidSignal
-    signalBlock:^RACSignal *(id input) {
-      @strongify(self)
-      if (![self.username isMobile]) {
-        return [RACSignal error:[self.class errorWithFailureReason:@"请输入正确的手机号"]];
-      }
-      return [[self executeFindPasswordCaptchaSignal] doNext:^(id x) {
-        self.counting = YES;
-        RACSignal *repetitiveEventSignal = [[RACSignal interval:1 onScheduler:RACScheduler.mainThreadScheduler] take:kCounterLength];
-        __block int repetCount = kCounterLength;
-        [repetitiveEventSignal subscribeNext:^(id x) {
-           self.counter = [@(--repetCount) stringValue];
-        } completed:^{
-          self.counter = @"获取验证码";
-          self.counting = NO;
-        }];
-      }];
-    }];
-  
-  return self;
+	self = [super init];
+	if (!self) {
+		return nil;
+	}
+	_server = server;
+	_username = @"";
+	_password = @"";
+	_captcha = @"";
+	_counter = @"获取验证码";
+	_agreeOnLicense = NO;
+	_counting = NO;
+	
+	@weakify(self)
+	_executeSignIn = [[RACCommand alloc] initWithEnabled:self.signInValidSignal
+		signalBlock:^RACSignal *(id input) {
+			@strongify(self)
+			if (![self.username isMobile]) {
+				return [RACSignal error:[self.class errorWithFailureReason:@"请输入正确的手机号"]];
+			} else if (![self.password isPassword]) {
+				return [RACSignal error:[self.class errorWithFailureReason:@"请输入8位以上数字和字母混合密码"]];
+			}
+		 
+			return [self executeSignInSignal];
+		}];
+	
+	_executeSignUp = [[RACCommand alloc] initWithEnabled:self.signUpValidSignal
+		signalBlock:^RACSignal *(id input) {
+			@strongify(self)
+			if (![self.username isMobile]) {
+				return [RACSignal error:[self.class errorWithFailureReason:@"请输入正确的手机号"]];
+			} else if (![self.captcha isCaptcha]) {
+				return [RACSignal error:[self.class errorWithFailureReason:@"请输入6位验证码"]];
+			} else if (![self.password isPassword]) {
+				return [RACSignal error:[self.class errorWithFailureReason:@"请输入8位以上数字和字母混合密码"]];
+			} else if (!self.agreeOnLicense) {
+				return [RACSignal error:[self.class errorWithFailureReason:@"请仔细阅读贷款协议"]];
+			}
+		 
+			return [self executeSignUpSignal];
+		}];
+	
+	_executeAgreeOnLicense = [[RACCommand alloc] initWithSignalBlock:^RACSignal *(id input) {
+		@strongify(self)
+		self.agreeOnLicense = !self.agreeOnLicense;
+		
+		return [RACSignal return:@(self.agreeOnLicense)];
+	}];
+	
+	_executeCaptcha = [[RACCommand alloc] initWithEnabled:self.captchaRequestValidSignal signalBlock:^RACSignal *(id input) {
+		@strongify(self)
+		if (![self.username isMobile]) {
+			return [RACSignal error:[self.class errorWithFailureReason:@"请输入正确的手机号"]];
+		}
+		return [[self executeCaptchaSignal]
+			doNext:^(id x) {
+				@strongify(self)
+				self.counting = YES;
+				RACSignal *repetitiveEventSignal = [[RACSignal interval:1 onScheduler:RACScheduler.mainThreadScheduler] take:kCounterLength];
+				__block int repetCount = kCounterLength;
+				[repetitiveEventSignal subscribeNext:^(id x) {
+					self.counter = [@(--repetCount) stringValue];
+				} completed:^{
+					self.counter = @"获取验证码";
+					self.counting = NO;
+				}];
+			}];
+		}];
+	
+	_executeFindPassword = [[RACCommand alloc] initWithEnabled:self.findPasswordValidSignal
+		signalBlock:^RACSignal *(id input) {
+			@strongify(self)
+			if (![self.username isMobile]) {
+				return [RACSignal error:[self.class errorWithFailureReason:@"请输入正确的手机号"]];
+			} else if (![self.captcha isCaptcha]) {
+				return [RACSignal error:[self.class errorWithFailureReason:@"请输入6位验证码"]];
+			} else if (![self.password isPassword]) {
+				return [RACSignal error:[self.class errorWithFailureReason:@"请输入8位以上数字和字母混合密码"]];
+			}
+		 
+			return [self executeFindPasswordSignal];
+		}];
+	
+	_executeFindPasswordCaptcha = [[RACCommand alloc] initWithEnabled:self.captchaRequestValidSignal
+		signalBlock:^RACSignal *(id input) {
+			@strongify(self)
+			if (![self.username isMobile]) {
+				return [RACSignal error:[self.class errorWithFailureReason:@"请输入正确的手机号"]];
+			}
+			return [[self executeFindPasswordCaptchaSignal] doNext:^(id x) {
+				self.counting = YES;
+				RACSignal *repetitiveEventSignal = [[RACSignal interval:1 onScheduler:RACScheduler.mainThreadScheduler] take:kCounterLength];
+				__block int repetCount = kCounterLength;
+				[repetitiveEventSignal subscribeNext:^(id x) {
+					 self.counter = [@(--repetCount) stringValue];
+				} completed:^{
+					self.counter = @"获取验证码";
+					self.counting = NO;
+				}];
+			}];
+		}];
+	
+	return self;
 }
 
 - (instancetype)init {
-  return [self initWithServer:MSFUtils.server];
+	return [self initWithServer:MSFUtils.server];
 }
 
 #pragma mark - Public
 
 - (RACSignal *)signInValidSignal {
-  return [RACSignal combineLatest:@[
-    RACObserve(self, username),
-    RACObserve(self, password)
-    ]
-    reduce:^id(NSString *username,NSString *password){
-      return @(username!=nil && password != nil);
-    }];
+	return [RACSignal
+		combineLatest:@[
+			RACObserve(self, username),
+			RACObserve(self, password)
+		]
+		reduce:^id(NSString *username, NSString *password){
+			return @(username!=nil && password != nil);
+		}];
 }
 
 - (RACSignal *)signUpValidSignal {
-  return [RACSignal combineLatest:@[
-    RACObserve(self, username),
-    RACObserve(self, password),
-    RACObserve(self, captcha),
-    ]
-    reduce:^id(NSString *username,NSString *password,NSString *captcha){
-      return @(username != nil && password != nil && captcha != nil);
-    }];
+	return [RACSignal
+		combineLatest:@[
+			RACObserve(self, username),
+			RACObserve(self, password),
+			RACObserve(self, captcha),
+		]
+		reduce:^id(NSString *username, NSString *password, NSString *captcha){
+			return @(username != nil && password != nil && captcha != nil);
+		}];
 }
 
 - (RACSignal *)captchaRequestValidSignal {
-  return [RACSignal combineLatest:@[
-    RACObserve(self, username),
-    RACObserve(self, counting)
-    ]
-    reduce:^id(NSString *username,NSNumber *counting){
-      return @(username != nil && !counting.boolValue);
-    }];
+	return [RACSignal
+		combineLatest:@[
+			RACObserve(self, username),
+			RACObserve(self, counting)
+		]
+		reduce:^id(NSString *username, NSNumber *counting){
+			return @(username != nil && !counting.boolValue);
+		}];
 }
 
 - (RACSignal *)findPasswordValidSignal {
-   return [RACSignal combineLatest:@[
-     RACObserve(self, username),
-     RACObserve(self, password),
-     RACObserve(self, captcha),
-     ]
-     reduce:^id(NSString *username,NSString *password,NSString *captcha){
-       return @(username != nil && password != nil && captcha != nil);
-     }];
+	 return [RACSignal
+		 combineLatest:@[
+			 RACObserve(self, username),
+			 RACObserve(self, password),
+			 RACObserve(self, captcha),
+		 ]
+		 reduce:^id(NSString *username, NSString *password, NSString *captcha){
+			 return @(username != nil && password != nil && captcha != nil);
+		 }];
 }
 
 #pragma mark - Private
 
 - (RACSignal *)executeSignInSignal {
-  MSFUser *user = [MSFUser userWithServer:self.server];
-  
-  return [[MSFClient
-    signInAsUser:user password:self.password phone:self.username captcha:self.captcha]
-    doNext:^(id x) {
-      [MSFUtils setHttpClient:x];
-    }];
+	MSFUser *user = [MSFUser userWithServer:self.server];
+	return [[MSFClient
+		signInAsUser:user password:self.password phone:self.username captcha:self.captcha]
+		doNext:^(id x) {
+			[MSFUtils setHttpClient:x];
+		}];
 }
 
 - (RACSignal *)executeSignUpSignal {
-  MSFUser *user = [MSFUser userWithServer:self.server];
-  
-  return [[MSFClient
-    signUpAsUser:user password:self.password phone:self.username captcha:self.captcha]
-    doNext:^(id x) {
-      [MSFUtils setHttpClient:x];
-    }];
+	MSFUser *user = [MSFUser userWithServer:self.server];
+	return [[MSFClient
+		signUpAsUser:user password:self.password phone:self.username captcha:self.captcha]
+		doNext:^(id x) {
+			[MSFUtils setHttpClient:x];
+		}];
 }
 
 - (RACSignal *)executeCaptchaSignal {
-  MSFClient *client = [[MSFClient alloc] initWithServer:self.server];
-  
-  return [client fetchSignUpCaptchaWithPhone:self.username];
+	MSFClient *client = [[MSFClient alloc] initWithServer:self.server];
+	return [client fetchSignUpCaptchaWithPhone:self.username];
 }
 
 - (RACSignal *)executeFindPasswordSignal {
-  MSFClient *client = [[MSFClient alloc] initWithServer:self.server];
-  
-  return [client resetPassword:self.password phone:self.username captcha:self.captcha];
+	MSFClient *client = [[MSFClient alloc] initWithServer:self.server];
+	return [client resetPassword:self.password phone:self.username captcha:self.captcha];
 }
 
 - (RACSignal *)executeFindPasswordCaptchaSignal {
-  MSFClient *client = [[MSFClient alloc] initWithServer:self.server];
-  
-  return [client fetchResetPasswordCaptchaWithPhone:self.username];
+	MSFClient *client = [[MSFClient alloc] initWithServer:self.server];
+	return [client fetchResetPasswordCaptchaWithPhone:self.username];
 }
 
 - (RACSignal *)executeLoginCaptchaSignal {
-  MSFClient *client = [[MSFClient alloc] initWithServer:self.server];
-  
-  return [client fetchLoginCaptchaWithPhone:self.username];
+	MSFClient *client = [[MSFClient alloc] initWithServer:self.server];
+	return [client fetchLoginCaptchaWithPhone:self.username];
 }
 
 + (NSError *)errorWithFailureReason:(NSString *)string {
-  return [NSError errorWithDomain:MSFAuthorizeErrorDomain code:0 userInfo:@{NSLocalizedFailureReasonErrorKey:string}];
+	return [NSError errorWithDomain:MSFAuthorizeErrorDomain code:0 userInfo:@{NSLocalizedFailureReasonErrorKey:string}];
 }
 
 @end
--- Classes/MSFUserViewController.m
+++ Classes/MSFUserViewController.m
@@ -21,10 +21,10 @@
 #import "UIButton+BottomTitles.h"
 #import "UIColor+Utils.h"
 
-@interface MSFUserViewController () <UITableViewDelegate,UITableViewDataSource>
+@interface MSFUserViewController () <UITableViewDelegate, UITableViewDataSource>
 
-@property(nonatomic,strong) NSArray *rowTitles;
-@property(nonatomic,strong) NSArray *icons;
+@property (nonatomic, strong) NSArray *rowTitles;
+@property (nonatomic, strong) NSArray *icons;
 
 @end
 
@@ -33,137 +33,137 @@
 #pragma mark - Lifecycle
 
 - (void)loadView {
-  self.tableView = [[UITableView alloc] initWithFrame:[UIScreen mainScreen].bounds style:UITableViewStyleGrouped];
+	self.tableView = [[UITableView alloc] initWithFrame:[UIScreen mainScreen].bounds style:UITableViewStyleGrouped];
 }
 
 - (void)viewDidLoad {
-  [super viewDidLoad];
-  [self.tableView registerClass:UITableViewCell.class forCellReuseIdentifier:@"Cell"];
-  self.tableView.delegate = self;
-  self.tableView.dataSource = self;
-  self.tableView.tableHeaderView = self.tableViewHeader;
-  self.tableView.backgroundColor = [UIColor darkBackgroundColor];
-  
-  self.rowTitles = @[
-    @"申请记录",
-    @"还款计划",
-    @"历史交易",
-    @"修改密码",
-    ];
-  self.icons = @[
-    [UIImage imageNamed:@"icon-account-apply"],
-    [UIImage imageNamed:@"icon-account-repay"],
-    [UIImage imageNamed:@"icon-account-history"],
-    [UIImage imageNamed:@"icon-account-edit"],
-  ];
+	[super viewDidLoad];
+	[self.tableView registerClass:UITableViewCell.class forCellReuseIdentifier:@"Cell"];
+	self.tableView.delegate = self;
+	self.tableView.dataSource = self;
+	self.tableView.tableHeaderView = self.tableViewHeader;
+	self.tableView.backgroundColor = [UIColor darkBackgroundColor];
+	
+	self.rowTitles = @[
+		@"申请记录",
+		@"还款计划",
+		@"历史交易",
+		@"修改密码",
+		];
+	self.icons = @[
+		[UIImage imageNamed:@"icon-account-apply"],
+		[UIImage imageNamed:@"icon-account-repay"],
+		[UIImage imageNamed:@"icon-account-history"],
+		[UIImage imageNamed:@"icon-account-edit"],
+	];
 }
 
 #pragma mark - Private
 
 - (UIView *)tableViewHeader {
-  UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth([UIScreen mainScreen].bounds), 170)];
-  view.backgroundColor = [UIColor whiteColor];
-  
-  UIImageView *backgroundView = UIImageView.new;
-  backgroundView.image = [UIImage imageNamed:@"bg-account-header"];
-  [view addSubview:backgroundView];
-  [backgroundView mas_makeConstraints:^(MASConstraintMaker *make) {
-    make.edges.equalTo(view);
-  }];
-  
-  UIButton *avatarButton = [UIButton buttonWithType:UIButtonTypeCustom];
-  [avatarButton setBackgroundImage:[UIImage imageNamed:@"icon-avatar-placeholder"] forState:UIControlStateNormal];
-  [view addSubview:avatarButton];
-  [avatarButton mas_makeConstraints:^(MASConstraintMaker *make) {
-    make.height.equalTo(@75);
-    make.width.equalTo(@75);
-    make.center.equalTo(view);
-  }];
-  
-  UILabel *label = UILabel.new;
-  label.textAlignment = NSTextAlignmentCenter;
-  label.textColor = [UIColor whiteColor];
-  label.text = [MSFUtils.httpClient user].name;
-  label.hidden = YES;
-  [view addSubview:label];
-  [label mas_makeConstraints:^(MASConstraintMaker *make) {
-    make.top.equalTo(avatarButton.mas_bottom).offset(-10);
-    make.centerX.equalTo(avatarButton);
-  }];
-  
-  @weakify(self)
-  [[avatarButton rac_signalForControlEvents:UIControlEventTouchUpInside]
-    subscribeNext:^(id x) {
-      @strongify(self)
-      MSFUserInfoViewController *userinfoViewController = [[MSFUserInfoViewController alloc] init];
-      userinfoViewController.hidesBottomBarWhenPushed = YES;
-      [self.navigationController pushViewController:userinfoViewController animated:YES];
-  }];
+	UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth([UIScreen mainScreen].bounds), 170)];
+	view.backgroundColor = [UIColor whiteColor];
+	
+	UIImageView *backgroundView = UIImageView.new;
+	backgroundView.image = [UIImage imageNamed:@"bg-account-header"];
+	[view addSubview:backgroundView];
+	[backgroundView mas_makeConstraints:^(MASConstraintMaker *make) {
+		make.edges.equalTo(view);
+	}];
+	
+	UIButton *avatarButton = [UIButton buttonWithType:UIButtonTypeCustom];
+	[avatarButton setBackgroundImage:[UIImage imageNamed:@"icon-avatar-placeholder"] forState:UIControlStateNormal];
+	[view addSubview:avatarButton];
+	[avatarButton mas_makeConstraints:^(MASConstraintMaker *make) {
+		make.height.equalTo(@75);
+		make.width.equalTo(@75);
+		make.center.equalTo(view);
+	}];
+	
+	UILabel *label = UILabel.new;
+	label.textAlignment = NSTextAlignmentCenter;
+	label.textColor = [UIColor whiteColor];
+	label.text = [MSFUtils.httpClient user].name;
+	label.hidden = YES;
+	[view addSubview:label];
+	[label mas_makeConstraints:^(MASConstraintMaker *make) {
+		make.top.equalTo(avatarButton.mas_bottom).offset(-10);
+		make.centerX.equalTo(avatarButton);
+	}];
+	
+	@weakify(self)
+	[[avatarButton rac_signalForControlEvents:UIControlEventTouchUpInside]
+		subscribeNext:^(id x) {
+			@strongify(self)
+			MSFUserInfoViewController *userinfoViewController = [[MSFUserInfoViewController alloc] init];
+			userinfoViewController.hidesBottomBarWhenPushed = YES;
+			[self.navigationController pushViewController:userinfoViewController animated:YES];
+	}];
  
-  return view;
+	return view;
 }
 
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-  return 4;
+	return 4;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
-  cell.textLabel.text  = self.rowTitles[indexPath.row];
-  cell.imageView.image = self.icons[indexPath.row];
-  cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-  
-  return cell;
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+	cell.textLabel.text  = self.rowTitles[indexPath.row];
+	cell.imageView.image = self.icons[indexPath.row];
+	cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+	
+	return cell;
 }
 
 #pragma mark - UITableViewDelegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-  [tableView deselectRowAtIndexPath:indexPath animated:YES];
-  switch (indexPath.row) {
-    case 0:
-      [self appliyStatusList:nil];
-      break;
-    case 1:
-      [self repayMentPlan:nil];
-      break;
-    case 2:
-      [self historyDetails:nil];
-      break;
-    case 3:
-      [self editUserInfo:nil];
-      break;
-    default:
-      break;
-  }
+	[tableView deselectRowAtIndexPath:indexPath animated:YES];
+	switch (indexPath.row) {
+		case 0:
+			[self appliyStatusList:nil];
+			break;
+		case 1:
+			[self repayMentPlan:nil];
+			break;
+		case 2:
+			[self historyDetails:nil];
+			break;
+		case 3:
+			[self editUserInfo:nil];
+			break;
+		default:
+			break;
+	}
 }
 
 #pragma mark - IBActions
 
 - (IBAction)repayMentPlan:(id)sender {
-  MSFRepaymentTableViewController *repaymentVC = [[MSFRepaymentTableViewController alloc]initWithStyle:UITableViewStylePlain];
-  repaymentVC.hidesBottomBarWhenPushed = YES;
-  [self.navigationController pushViewController:repaymentVC animated:YES];
+	MSFRepaymentTableViewController *repaymentVC = [[MSFRepaymentTableViewController alloc]initWithStyle:UITableViewStylePlain];
+	repaymentVC.hidesBottomBarWhenPushed = YES;
+	[self.navigationController pushViewController:repaymentVC animated:YES];
 }
 
 - (IBAction)historyDetails:(id)sender {
-  MSFTradeTableViewController *tradeVC = [[MSFTradeTableViewController alloc]initWithStyle:UITableViewStylePlain];
-  tradeVC.hidesBottomBarWhenPushed = YES;
-  [self.navigationController pushViewController:tradeVC animated:YES];
+	MSFTradeTableViewController *tradeVC = [[MSFTradeTableViewController alloc]initWithStyle:UITableViewStylePlain];
+	tradeVC.hidesBottomBarWhenPushed = YES;
+	[self.navigationController pushViewController:tradeVC animated:YES];
 }
 
 - (IBAction)editUserInfo:(id)sender {
-  MSFEditPasswordViewController *editUserinfoViewController = [[MSFEditPasswordViewController alloc] init];
-  editUserinfoViewController.hidesBottomBarWhenPushed = YES;
-  [self.navigationController pushViewController:editUserinfoViewController animated:YES];
+	MSFEditPasswordViewController *editUserinfoViewController = [[MSFEditPasswordViewController alloc] init];
+	editUserinfoViewController.hidesBottomBarWhenPushed = YES;
+	[self.navigationController pushViewController:editUserinfoViewController animated:YES];
 }
 
 - (IBAction)appliyStatusList:(id)sender {
-  MSFLoanListViewController *loanListVC = [[MSFLoanListViewController alloc] init];
-  loanListVC.hidesBottomBarWhenPushed = YES;
-  [self.navigationController pushViewController:loanListVC animated:YES];
+	MSFLoanListViewController *loanListVC = [[MSFLoanListViewController alloc] init];
+	loanListVC.hidesBottomBarWhenPushed = YES;
+	[self.navigationController pushViewController:loanListVC animated:YES];
 }
 
 @end
