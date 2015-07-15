315285a00ac4c45aad65c2f2873da034b2fa5812
Zēng Liàng <ObjcZL@gmail.com>
2015-07-15 19:53:57 +0800

    Fix 完善资料后没有更新贷款资料

--- Classes/MSFTabBarController.m
+++ Classes/MSFTabBarController.m
@@ -66,6 +66,12 @@
 			self.viewModel.formsViewModel.active = NO;
 			[self authenticatedControllers];
 		}];
+	[[[NSNotificationCenter defaultCenter]
+		rac_addObserverForName:@"MSFClozeViewModelDidUpdateNotification" object:nil]
+		subscribeNext:^(id x) {
+			@strongify(self)
+			[self.viewModel.signInCommand execute:nil];
+		}];
 	
 	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"提示" message:@"已在另一设备上登录，如非本人操作请立即修改密码" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
 	alertView.delegate = self;
