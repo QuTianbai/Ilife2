0dda8247759b047ecd2ef9c96859f97e660638ec
Zēng Liàng <ObjcZL@gmail.com>
2015-07-15 10:15:29 +0800

    Add MSFTabBarController

---
+++ Classes/MSFTabBarController.h
@@ -0,0 +1,12 @@
+//
+// MSFTabBarController.h
+//
+// Copyright (c) 2015 Zēng Liàng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSFTabBarController : UITabBarController
+
+
+@end
---
+++ Classes/MSFTabBarController.m
@@ -0,0 +1,12 @@
+//
+// MSFTabBarController.m
+//
+// Copyright (c) 2015 Zēng Liàng. All rights reserved.
+//
+
+#import "MSFTabBarController.h"
+
+@implementation MSFTabBarController
+
+
+@end
