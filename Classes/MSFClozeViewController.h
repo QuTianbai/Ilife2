acd3ed1387ca3de56a1880a8cad5913d2cf03f7f
Zēng Liàng <ObjcZL@gmail.com>
2015-07-16 16:17:37 +0800

    Improve 完善资料storyboard

--- Classes/MSFClozeViewController.h
+++ Classes/MSFClozeViewController.h
@@ -9,6 +9,19 @@
 /**
  *	完善个人信息-实名认证
  */
-@interface MSFClozeViewController : UIViewController
+@interface MSFClozeViewController : UITableViewController
+
+@property(nonatomic,weak) IBOutlet UITextField *name;
+@property(nonatomic,weak) IBOutlet UITextField *card;
+@property(nonatomic,weak) IBOutlet UITextField *expired;
+@property(nonatomic,weak) IBOutlet UIButton *permanentButton;
+@property(nonatomic,weak) IBOutlet UIButton *datePickerButton;
+@property(nonatomic,weak) IBOutlet UIButton *submitButton;
+@property(nonatomic,weak) IBOutlet UITextField *bankName;
+@property(nonatomic,weak) IBOutlet UITextField *bankNO;
+@property(nonatomic,weak) IBOutlet UITextField *bankAddress;
+@property(nonatomic,weak) IBOutlet UIButton *bankNameButton;
+@property(nonatomic,weak) IBOutlet UIButton *bankNOButton;
+@property(nonatomic,weak) IBOutlet UIButton *bankAddressButton;
 
 @end
