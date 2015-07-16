2424a0a96cc1ab832b8dcaed7ebf91614f6dc172
Zēng Liàng <ObjcZL@gmail.com>
2015-07-16 15:38:00 +0800

    Rename loginviewcontroller to signinviewcontroller

--- Classes/MSFSignInViewController.h
+++ Classes/MSFSignInViewController.h
@@ -7,12 +7,18 @@
 #import <UIKit/UIKit.h>
 
 /**
- *	登录－输入
+ *	登录
  */
+
+@class MSFAuthorizeViewModel;
+
 @interface MSFSignInViewController : UITableViewController
 
 @property(nonatomic,weak) IBOutlet UITextField *username;
 @property(nonatomic,weak) IBOutlet UITextField *password;
 @property(nonatomic,weak) IBOutlet UIButton *signInButton;
+@property (nonatomic, strong, readonly) MSFAuthorizeViewModel *viewModel;
+
+- (instancetype)initWithViewModel:(MSFAuthorizeViewModel *)viewModel;
 
 @end
