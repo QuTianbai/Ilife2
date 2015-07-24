d7441a292daf944ddbba8a78984439cefc7c67fe
Zēng Liàng <ObjcZL@gmail.com>
2015-07-24 16:43:25 +0800

    Update 注册ViewModel

--- Classes/MSFSignUpViewController.m
+++ Classes/MSFSignUpViewController.m
@@ -37,7 +37,6 @@ static NSString *const MSFAutoinputDebuggingEnvironmentKey = @"INPUT_AUTO_DEBUG"
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	//TODO: 增加初始化绑定viewmodell方法
 	self.tableView.backgroundColor = [UIColor colorWithWhite:0.98 alpha:1];
 	self.backgroundView.layer.masksToBounds = YES;
 	self.backgroundView.layer.cornerRadius = 5;
@@ -47,22 +46,43 @@ static NSString *const MSFAutoinputDebuggingEnvironmentKey = @"INPUT_AUTO_DEBUG"
 	if (NSProcessInfo.processInfo.environment[MSFAutoinputDebuggingEnvironmentKey] != nil) {
 		self.username.text = @"18223959242";
 	}
-	self.viewModel = [[MSFAuthorizeViewModel alloc] init];
-	RAC(self.viewModel, username) = self.username.rac_textSignal;
-	RAC(self.viewModel, password) = self.password.rac_textSignal;
-	RAC(self.viewModel, captcha) = self.captcha.rac_textSignal;
-	RAC(self.counterLabel, text) = RACObserve(self.viewModel, counter);
+}
+
+#pragma mark - Private
+
+- (void)initialize {
+	@weakify(self)
+	[self.username.rac_textSignal subscribeNext:^(id x) {
+		@strongify(self)
+		self.viewModel.username = x;
+	}];
+	[self.password.rac_textSignal subscribeNext:^(id x) {
+		@strongify(self)
+		self.viewModel.password = x;
+	}];
+	[self.captcha.rac_textSignal subscribeNext:^(id x) {
+		@strongify(self)
+		self.viewModel.captcha = x;
+	}];
+	[RACObserve(self.viewModel, counter) subscribeNext:^(id x) {
+		@strongify(self)
+		self.counterLabel.text = x;
+	}];
+	[RACObserve(self.viewModel, agreeOnLicense) subscribeNext:^(id x) {
+		@strongify(self)
+		self.agreeButton.selected = [x boolValue];
+	}];
 	
-	RAC(self.counterLabel, textColor) = [self.viewModel.captchaRequestValidSignal
-		map:^id(NSNumber *valid) {
-			return valid.boolValue ? UIColor.darkGrayColor: UIColor.lightGrayColor;
-		}];
+	[self.viewModel.captchaRequestValidSignal subscribeNext:^(NSNumber *value) {
+		@strongify(self)
+		self.counterLabel.textColor = value.boolValue ? UIColor.darkGrayColor: UIColor.lightGrayColor;
+	}];
+	
 	self.iAgreeButton.rac_command = self.viewModel.executeAgreeOnLicense;
 	self.agreeButton.rac_command = self.viewModel.executeAgreeOnLicense;
 	RAC(self.agreeButton, selected) = RACObserve(self.viewModel, agreeOnLicense);
 	
 	self.commitButton.rac_command = self.viewModel.executeSignUp;
-	@weakify(self)
 	[self.commitButton.rac_command.executionSignals subscribeNext:^(RACSignal *signUpSignal) {
 		@strongify(self)
 		[self.view endEditing:YES];
@@ -96,7 +116,11 @@ static NSString *const MSFAutoinputDebuggingEnvironmentKey = @"INPUT_AUTO_DEBUG"
 	}];
 }
 
+#pragma mark - MSFReactiveView
+
 - (void)bindViewModel:(id)viewModel {
+	self.viewModel = viewModel;
+	[self initialize];
 }
 
 @end
