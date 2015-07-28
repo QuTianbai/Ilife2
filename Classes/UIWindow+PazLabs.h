d2514ccd49367df68b6408899afa10d2fdcb421b
Zēng Liàng <ObjcZL@gmail.com>
2015-07-28 10:19:16 +0800

    Update services push viewmodel

---
+++ Classes/UIWindow+PazLabs.h
@@ -0,0 +1,13 @@
+//
+// UIWindow+PazLabs.h
+//
+// Copyright (c) 2015 Zēng Liàng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWindow (PazLabs)
+
+- (UIViewController *)visibleViewController;
+
+@end
