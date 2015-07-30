8701f8b96704eec0633b716c38e7e67f50b4649c
Zēng Liàng <ObjcZL@gmail.com>
2015-07-30 19:46:42 +0800

    Improve 程序启动获取时间戳的等待界面

---
+++ Classes/MSFActivityIndicatorViewController.m
@@ -0,0 +1,11 @@
+//
+// MSFActivityIndicatorViewController.m
+//
+// Copyright (c) 2015 Zēng Liàng. All rights reserved.
+//
+
+#import "MSFActivityIndicatorViewController.h"
+
+@implementation MSFActivityIndicatorViewController
+
+@end
