488e12076ddc47e7de2bfd876bdfaf0704940b83
xbm <baimiao.xu@msfinance.cn>
2015-07-31 09:28:27 +0800

    贷款

--- Classes/MSFClient+MSFLocation.m
+++ Classes/MSFClient+MSFLocation.m
@@ -9,17 +9,31 @@
 #import "MSFClient+MSFLocation.h"
 #import "RACSignal+MSFClientAdditions.h"
 #import "MSFLocation.h"
+#import <AFNetworking/AFNetworking.h>
 
 @implementation MSFClient (MSFLocation)
 
 - (RACSignal *)fetchLocationAdress:(CLLocationCoordinate2D)coordinate {
   NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://api.map.baidu.com/geocoder/v2/?ak=sTLArkR5mCiQrGcflln8li7c&callback=renderReverse&location=%f,%f&output=json&pois=1", coordinate.latitude, coordinate.longitude]];
-  //NSURLRequest *request = [NSURLRequest requestWithURL:url];
-  //NSURLRequest *request = [[NSURLRequest alloc]initWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10];
-  NSMutableURLRequest *request = [self.requestSerializer requestWithMethod:@"GET"
-                                                                 URLString:url.absoluteString
-                                                                parameters:nil
-                                                                     error:nil];
+  NSURLRequest *request = [NSURLRequest requestWithURL:url];
+  
+  AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc]initWithRequest:request];
+  [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+    NSString *dataStr = [operation.responseString stringByReplacingOccurrencesOfString:@"renderReverse&&renderReverse(" withString:@""] ;
+    NSString *str = [dataStr substringToIndex:dataStr.length - 1];
+    NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *dict = [NSJSONSerialization  JSONObjectWithData:data options:0 error:nil];
+    NSLog(@"获取到的数据为：%@", dict);
+  }failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+    NSLog(@"发生错误！%@", error);
+  }];
+  NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+  [queue addOperation:operation];  
+ // NSURLRequest *request = [[NSURLRequest alloc]initWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10];
+//  NSMutableURLRequest *request = [self.requestSerializer requestWithMethod:@"GET"
+//                                                                 URLString:url.relativeString
+//                                                                parameters:nil
+//                                                                     error:nil];
   return [[self enqueueRequest:request resultClass:MSFLocation.class] msf_parsedResults];
   
 }
