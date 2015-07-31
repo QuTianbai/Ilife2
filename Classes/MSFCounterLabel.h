79cd49af1e473ee733c88c1e43b097f483448611
zhaoyong <zhaoyo31@icloud.com>
2015-07-31 14:07:50 +0800

    add CounterLabel

---
+++ Classes/MSFCounterLabel.h
@@ -0,0 +1,13 @@
+//
+//  MSFCounterLabel.h
+//  Finance
+//
+//  Created by 赵勇 on 7/31/15.
+//  Copyright (c) 2015 MSFINANCE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSFCounterLabel : UILabel
+
+@end
---
+++ Classes/MSFCounterLabel.m
@@ -0,0 +1,21 @@
+//
+//  MSFCounterLabel.m
+//  Finance
+//
+//  Created by 赵勇 on 7/31/15.
+//  Copyright (c) 2015 MSFINANCE. All rights reserved.
+//
+
+#import "MSFCounterLabel.h"
+
+@implementation MSFCounterLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
