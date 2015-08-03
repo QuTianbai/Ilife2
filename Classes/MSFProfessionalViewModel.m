6dc8ea0e6bac354a24a9ed907dfeabdc5e6fbb57
xbm <baimiao.xu@msfinance.cn>
2015-08-03 10:55:44 +0800

    居住地至少输入两项验证，在职人员部门不显示修正

--- Classes/MSFProfessionalViewModel.m
+++ Classes/MSFProfessionalViewModel.m
@@ -102,13 +102,13 @@
 			*stop = YES;
 		}
 	}];
-	NSArray *department = [MSFSelectKeyValues getSelectKeys:@"professional"];
-	[department enumerateObjectsUsingBlock:^(MSFSelectKeyValues *obj, NSUInteger idx, BOOL *stop) {
-		if ([obj.code isEqualToString:self.model.department]) {
-			self.department = obj;
-			*stop = YES;
-		}
-	}];
+//	NSArray *department = [MSFSelectKeyValues getSelectKeys:@"professional"];
+//	[department enumerateObjectsUsingBlock:^(MSFSelectKeyValues *obj, NSUInteger idx, BOOL *stop) {
+//		if ([obj.code isEqualToString:self.model.department]) {
+//			self.department = obj;
+//			*stop = YES;
+//		}
+//	}];
 }
 
 - (void)initialize {
@@ -165,12 +165,13 @@
 		self.model.companyType = object.code;
 		self.natureTitle = object.text;
 	}];
-	
-	[RACObserve(self, department) subscribeNext:^(MSFSelectKeyValues *object) {
-		@strongify(self)
-		self.model.department = object.code;
-		self.departmentTitle = object.text;
-	}];
+  
+  RACChannelTo(self, departmentTitle) = RACChannelTo(self.model, department);
+//	[RACObserve(self, department) subscribeNext:^(MSFSelectKeyValues *object) {
+//		@strongify(self)
+//		self.model.department = object.code;
+//		self.departmentTitle = object.text;
+//	}];
 	
 	[RACObserve(self, position) subscribeNext:^(MSFSelectKeyValues *object) {
 		@strongify(self)
