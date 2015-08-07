4cee64983aa0d8d72c91e0d157e057020fe7aa26
Zēng Liàng <ObjcZL@gmail.com>
2015-08-07 11:19:44 +0800

    Add NSURLRequest+RequestWithIgnoreSSL

---
+++ Classes/NSURLRequest+RequestWithIgnoreSSL.h
@@ -0,0 +1,12 @@
+//
+// NSURLRequest+RequestWithIgnoreSSL.h
+//
+// Copyright (c) 2015 Zēng Liàng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSURLRequest (RequestWithIgnoreSSL)
+
+
+@end
---
+++ Classes/NSURLRequest+RequestWithIgnoreSSL.m
@@ -0,0 +1,16 @@
+//
+// NSURLRequest+RequestWithIgnoreSSL.m
+//
+// Copyright (c) 2015 Zēng Liàng. All rights reserved.
+//
+
+#import "NSURLRequest+RequestWithIgnoreSSL.h"
+
+@implementation NSURLRequest (RequestWithIgnoreSSL)
+
++ (BOOL)allowsAnyHTTPSCertificateForHost:(NSString *)host {
+	return YES; 
+} 
+
+
+@end
