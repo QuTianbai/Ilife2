95b9b0033ee08b8bc51c8dfd7bad17a1b5f02426
Zēng Liàng <ObjcZL@gmail.com>
2015-08-25 11:43:35 +0800

    Add spec

---
+++ Tests/MSFProfessionalViewModelSpec.m
@@ -0,0 +1,19 @@
+//
+// MSFProfessionalViewModelSpec.m
+//
+// Copyright (c) 2015 Zēng Liàng. All rights reserved.
+//
+
+#import "MSFProfessionalViewModel.h"
+
+QuickSpecBegin(MSFProfessionalViewModelSpec)
+
+it(@"should initialize", ^{
+  // given
+  
+  // when
+  
+  // then
+});
+
+QuickSpecEnd
