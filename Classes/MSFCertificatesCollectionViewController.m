505a721ccf0c589886e17e7286ed257944cd95e7
zhaoyong <zhaoyo31@icloud.com>
2015-09-08 14:28:39 +0800

    修复6、6+collectionViewCell背景线条错乱的bug

--- Classes/MSFCertificatesCollectionViewController.m
+++ Classes/MSFCertificatesCollectionViewController.m
@@ -117,7 +117,7 @@ UICollectionViewDelegateFlowLayout>
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section {
 	if (section == 0 && !self.optional) {
-		return CGSizeMake([UIScreen mainScreen].bounds.size.width, 90);
+		return CGSizeMake([UIScreen mainScreen].bounds.size.width, 100);
 	} else {
 		return CGSizeZero;
 	}
@@ -126,7 +126,7 @@ UICollectionViewDelegateFlowLayout>
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
 	CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
 	if (indexPath.section == 0) {
-		return CGSizeMake(screenWidth * 0.5, screenWidth * 0.35);
+		return CGSizeMake(screenWidth * 0.5, floor(screenWidth * 0.35));
 	} else {
 		return CGSizeMake(screenWidth, 44);
 	}
