4be570cf5ab5b6e7c6b5ec04f4274a0d141ba1c1
zhaoyong <zhaoyo31@icloud.com>
2015-09-14 11:24:36 +0800

    提交图片组织屏幕触摸时间

--- Classes/MSFPhotoUploadCollectionViewController.m
+++ Classes/MSFPhotoUploadCollectionViewController.m
@@ -83,7 +83,7 @@ MWPhotoBrowserDelegate>
 			[self.navigationController popViewControllerAnimated:YES];
 			return;
 		}
-		[SVProgressHUD showWithStatus:@"正在提交..." maskType:SVProgressHUDMaskTypeNone];
+		[SVProgressHUD showWithStatus:@"正在提交..." maskType:SVProgressHUDMaskTypeClear];
 		[signal subscribeNext:^(id x) {
 			[SVProgressHUD showSuccessWithStatus:@"上传成功"];
 			dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
