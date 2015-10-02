3ff22d72e7629f6dcd25d83e077aa5c9d3603c7e
ZeÃÑng LiaÃÄng <ObjcZL@gmail.com>
2015-10-02 09:42:47 +0800

    开发环境下增加调试信息输出窗口

--- Finance/AppDelegate.m
+++ Finance/AppDelegate.m
@@ -56,7 +56,7 @@
 	[Fabric with:@[CrashlyticsKit]];
   [SVProgressHUD setBackgroundColor:[UIColor colorWithHue:0 saturation:0 brightness:0.95 alpha:0.8]];
 	
-#if TEST
+#if TEST || DEBUG
 	[BugshotKit enableWithNumberOfTouches:2 performingGestures:(BSKInvocationGestureSwipeFromRightEdge | BSKInvocationGestureSwipeUp) feedbackEmailAddress:@"liang.zeng@msxf.com"];
 #endif
 	
