903a18f0f28c36bcdeccc1fd07256de113e763c1
Zēng Liàng <ObjcZL@gmail.com>
2015-10-02 20:25:41 +0800

    Improve login segmented view

---
+++ Classes/MSFSignInSegmentedView.m
@@ -0,0 +1,15 @@
+//
+// MSFSignInSegmentedView.m
+//
+// Copyright (c) 2015 Zēng Liàng. All rights reserved.
+//
+
+#import "MSFSignInSegmentedView.h"
+
+@implementation MSFSignInSegmentedView
+
+- (void)setFrame:(CGRect)frame {
+	[super setFrame:CGRectMake(0.0, 0.0, CGRectGetWidth([UIScreen mainScreen].bounds), 44.0)];
+}
+
+@end
