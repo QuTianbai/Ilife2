4a8a9aea54f91912af299bcb86b3cc1878e7de78
Zēng Liàng <ObjcZL@gmail.com>
2015-10-21 16:16:58 +0800

    Fix 中文名字限制长度

--- Classes/MSFSignInPhoneViewController.m
+++ Classes/MSFSignInPhoneViewController.m
@@ -43,11 +43,21 @@
 	RAC(self, citizenID.text) = RACObserve(self, viewModel.card);
 	RAC(self, name.text) = RACObserve(self, viewModel.name);
 	
-	[self.name.rac_textSignal subscribeNext:^(id x) {
-		@strongify(self)
-		if ([x length] > MSFAuthorizeNameMaxLength) self.name.text = [x substringToIndex:MSFAuthorizeNameMaxLength];
-		self.viewModel.name = self.name.text;
-	}];
+	RAC(self.name, text) = [[[self.name.rac_textSignal
+		map:^id(NSString *value) {
+			return [value stringByTrimmingCharactersInSet:[[NSCharacterSet chineseCharacterSet] invertedSet]];
+		}]
+		map:^id(NSString *value) {
+			if (value.length > MSFAuthorizeNameMaxLength) {
+				return [value substringToIndex:MSFAuthorizeNameMaxLength];
+			} else {
+				return value;
+			}
+		}]
+		doNext:^(id x) {
+			@strongify(self)
+			self.viewModel.name = x;
+		}];
 	
 	[self.citizenID.rac_textSignal subscribeNext:^(id x) {
 		@strongify(self)
