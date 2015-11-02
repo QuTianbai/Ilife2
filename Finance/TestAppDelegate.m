d0e9e8ae067bc1eedbd608c0a974c1f7dc6fcf05
Zēng Liàng <ObjcZL@gmail.com>
2015-11-02 11:31:11 +0800

    Fix ios7 TestAppDelegate

--- Finance/TestAppDelegate.m
+++ Finance/TestAppDelegate.m
@@ -10,6 +10,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+	self.window.rootViewController = [[UIViewController alloc] init];
   [self.window makeKeyAndVisible];
   
   return YES;
