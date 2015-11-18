f22ff725e222aa4a1f98335f0e44f82ca12404f2
zhaoyong <zhaoyo31@icloud.com>
2015-11-18 10:23:25 +0800

    commit

--- Classes/MSFServer.m
+++ Classes/MSFServer.m
@@ -27,8 +27,8 @@ static NSString *URLString(void) {
 	return url ?: @"http://api3.msxf.test";
 }
 
-NSString *const MSFServerDotComAPIEndpoint = @"http://api.msxf.lotest";
-NSString *const MSFServerDotComBaseWebURL = @"http://api.msxf.lotest";
+NSString *const MSFServerDotComAPIEndpoint = @"http://api2.msxf.lotest";
+NSString *const MSFServerDotComBaseWebURL = @"http://api2.msxf.lotest";
 
 #elif TP
 
