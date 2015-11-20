192a8d7bd4be7dccd7931b9f46249a93a8428861
Zēng Liàng <ObjcZL@gmail.com>
2015-11-20 11:31:14 +0800

    Refactor attachment viewmodel

--- Classes/MSFAttachmentViewModel.m
+++ Classes/MSFAttachmentViewModel.m
@@ -17,15 +17,95 @@
 @interface MSFAttachmentViewModel ()
 
 @property (nonatomic, weak) id <MSFViewModelServices> services;
-@property (nonatomic, strong, readwrite) NSURL *fileURL;
-@property (nonatomic, strong, readwrite) NSURL *thumbURL;
-@property (nonatomic, strong, readwrite) NSString *fileName;
+@property (nonatomic, assign, readwrite) BOOL isUploaded;
 
 @end
 
 @implementation MSFAttachmentViewModel
 
 #pragma mark - Lifecycle
+
+- (instancetype)initWithModel:(MSFAttachment *)model services:(id <MSFViewModelServices>)services {
+  self = [super init];
+  if (!self) {
+    return nil;
+  }
+	_services = services;
+	_attachment = model;
+	_removeEnabled = !self.attachment.isPlaceholder;
+	RAC(self, thumbURL) = RACObserve(self, attachment.thumbURL);
+	
+	@weakify(self)
+	[RACObserve(self, attachment.isUpload) subscribeNext:^(id x) {
+		@strongify(self)
+		self.isUploaded = [x boolValue];
+	}];
+	
+	_takePhotoCommand = [[RACCommand alloc] initWithEnabled:self.takePhotoValidSignal signalBlock:^RACSignal *(id input) {
+		@strongify(self)
+		return [self takePhotoSignal];
+	}];
+	_takePhotoCommand.allowsConcurrentExecution = YES;
+	_uploadAttachmentCommand = [[RACCommand alloc] initWithEnabled:self.uploadValidSignal signalBlock:^RACSignal *(id input) {
+		@strongify(self)
+		return [[self.services.httpClient uploadAttachment:self.attachment] doNext:^(id x) {
+			[self.attachment mergeAttachment:x];
+		}];
+	}];
+	_removeCommand = [[RACCommand alloc] initWithEnabled:RACObserve(self, removeEnabled) signalBlock:^RACSignal *(id input) {
+		return [RACSignal empty];
+	}];
+
+	return self;
+}
+
+#pragma mark - Custom Accessors 
+
+- (RACSignal *)takePhotoValidSignal {
+	return [RACSignal combineLatest:@[RACObserve(self, attachment.isPlaceholder)] reduce:^id(NSNumber *placeholder){
+		return @(placeholder.boolValue);
+	}];
+}
+
+- (RACSignal *)uploadValidSignal {
+	return [RACSignal combineLatest:@[
+		RACObserve(self, isUploaded),
+		RACObserve(self, removeEnabled),
+	]
+	reduce:^id(NSNumber *uploaded, NSNumber *enabled){
+		return @(!uploaded.boolValue && enabled.boolValue);
+	}];
+}
+
+#pragma mark - Private
+
+- (RACSignal *)takePhotoSignal {
+	return [[self.services msf_takePictureSignal] map:^id(UIImage *image) {
+		NSString *name = [@([[NSDate date] timeIntervalSince1970]) stringValue].md5;
+		NSString *path = [NSTemporaryDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.jpg", name]];
+		CGSize size = image.size;
+		if (size.width > 800 || size.height > 800) {
+			CGFloat scale = size.width > size.height ? 800.0 / size.width : 800.0 / size.height;
+			size = size.width > size.height ? CGSizeMake(800, size.height * scale) : CGSizeMake(size.width * scale, 800);
+		}
+		UIImage *x = [image resizedImage:size interpolationQuality:kCGInterpolationDefault];
+		[[NSFileManager defaultManager] createFileAtPath:path contents:UIImageJPEGRepresentation(x, .7) attributes:nil];
+		NSURL *fileURL = [NSURL fileURLWithPath:path];
+		
+		return [[MSFAttachment alloc] initWithFileURL:fileURL
+			applicationNo:self.attachment.applicationNo
+			elementType:self.attachment.type
+			elementName:self.attachment.name];
+	}];
+}
+
+@end
+
+#pragma mark - Deprecated
+
+@implementation MSFAttachmentViewModel (Deprecated)
+
+@dynamic fileURL;
 
 - (instancetype)initWthAttachment:(MSFAttachment *)attachment viewModel:(MSFApplyCashVIewModel *)viewModel {
   self = [super init];
@@ -36,7 +116,6 @@
 	_attachment = attachment;
 	_removeEnabled = !attachment.isPlaceholder;
 	RACChannelTo(self, thumbURL) = RACChannelTo(self, attachment.thumbURL);
-	RACChannelTo(self, fileURL) = RACChannelTo(self, attachment.fileURL);
 	
 	_takePhotoCommand = [[RACCommand alloc] initWithEnabled:self.takePhotoValidSignal signalBlock:^RACSignal *(id input) {
 		return [self takePhotoSignal];
@@ -49,20 +128,10 @@
 			[self.attachment mergeValueForKey:@keypath(MSFAttachment.new, fileName) fromModel:x];
 		}];
 	}];
-	_downloadAttachmentCommand = [[RACCommand alloc] initWithEnabled:self.downloadValidSignal signalBlock:^RACSignal *(id input) {
-		return [[self.services.httpClient downloadAttachment:self.attachment] doNext:^(id x) {
-			NSString *name = [@([[NSDate date] timeIntervalSince1970]) stringValue].md5;
-			NSString *path = [NSTemporaryDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.jpg", name]];
-			[[NSFileManager defaultManager] createFileAtPath:path contents:UIImageJPEGRepresentation(x, .7) attributes:nil];
-			self.attachment.thumbURL = [NSURL fileURLWithPath:path];
-			self.attachment.fileURL = [NSURL URLWithString:path];
-		}];
-	}];
-	
-	_removeCommand = [[RACCommand alloc] initWithEnabled:[RACSignal return:@(self.removeEnabled)] signalBlock:^RACSignal *(id input) {
+	_removeCommand = [[RACCommand alloc] initWithEnabled:RACObserve(self, removeEnabled) signalBlock:^RACSignal *(id input) {
 		return [RACSignal empty];
 	}];
-  
+	
   return self;
 }
 
@@ -77,57 +146,20 @@
   return self;
 }
 
-#pragma mark - Custom Accessors 
-
-- (RACSignal *)takePhotoValidSignal {
-	return [RACSignal combineLatest:@[RACObserve(self, attachment.isPlaceholder)] reduce:^id(NSNumber *placeholder){
-		return @(placeholder.boolValue);
-	}];
-}
-
-- (RACSignal *)uploadValidSignal {
-	return [RACSignal combineLatest:@[
-		RACObserve(self, attachment.isPlaceholder),
-		RACObserve(self, attachment.objectID),
-	]
-	reduce:^id(NSNumber *placeholder, NSString *objectID){
-		return @(!placeholder.boolValue && !objectID);
+- (RACCommand *)downloadAttachmentCommand {
+	return [[RACCommand alloc] initWithEnabled:self.downloadValidSignal signalBlock:^RACSignal *(id input) {
+		return [[self.services.httpClient downloadAttachment:self.attachment] doNext:^(id x) {
+			NSString *name = [@([[NSDate date] timeIntervalSince1970]) stringValue].md5;
+			NSString *path = [NSTemporaryDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.jpg", name]];
+			[[NSFileManager defaultManager] createFileAtPath:path contents:UIImageJPEGRepresentation(x, .7) attributes:nil];
+			self.attachment.thumbURL = [NSURL fileURLWithPath:path];
+			self.attachment.fileURL = [NSURL URLWithString:path];
+		}];
 	}];
 }
 
 - (RACSignal *)downloadValidSignal {
-	return [RACSignal combineLatest:@[
-		RACObserve(self, attachment.isPlaceholder),
-		RACObserve(self, attachment.name),
-	]
-	reduce:^id(NSNumber *placeholder, NSString *file){
-		NSString *path = [NSTemporaryDirectory() stringByAppendingString:[NSString stringWithFormat:@"%@", file]];
-		BOOL exist = [[NSFileManager defaultManager] fileExistsAtPath:path];
-		return @(!placeholder.boolValue && !exist);
-	}];
-}
-
-- (BOOL)isUploaded {
-	return self.attachment.objectID != nil;
-}
-
-#pragma mark - Private
-
-- (RACSignal *)takePhotoSignal {
-	return [[self.services msf_takePictureSignal] doNext:^(UIImage *image) {
-		NSString *name = [@([[NSDate date] timeIntervalSince1970]) stringValue].md5;
-		NSString *path = [NSTemporaryDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.jpg", name]];
-		CGSize size = image.size;
-		if (size.width > 800 || size.height > 800) {
-			CGFloat scale = size.width > size.height ? 800.0 / size.width : 800.0 / size.height;
-			size = size.width > size.height ? CGSizeMake(800, size.height * scale) : CGSizeMake(size.width * scale, 800);
-		}
-		UIImage *x = [image resizedImage:size interpolationQuality:kCGInterpolationDefault];
-		[[NSFileManager defaultManager] createFileAtPath:path contents:UIImageJPEGRepresentation(x, .7) attributes:nil];
-		self.fileURL = [NSURL fileURLWithPath:path];
-		if (!self.attachment.isPlaceholder)
-			self.thumbURL = [NSURL fileURLWithPath:path];
-	}];
+	return [RACSignal empty];
 }
 
 @end
