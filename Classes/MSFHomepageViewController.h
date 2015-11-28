b0dfaca486f4a984772f8030125da5add6a71dbe
zhaoyong <zhaoyo31@icloud.com>
2015-11-28 14:39:36 +0800

    sort code , delete MSFBannerViewController / MSFInfinityScroll

--- Classes/MSFHomepageViewController.h
+++ Classes/MSFHomepageViewController.h
@@ -13,13 +13,6 @@
 // 主页的viewModel
 @property (nonatomic, strong, readonly) MSFHomepageViewModel *viewModel;
 
-/*
- * 用viewModel初始化
- *
- * viewModel：主页的viewModel
- *
- * 返回MSFHomepageViewController对象
- */
 - (instancetype)initWithViewModel:(MSFHomepageViewModel *)viewModel;
 
 @end
