11a845ced539cd9d632251509618431cb7bb211f
zhaoyong <zhaoyo31@icloud.com>
2015-12-03 17:11:14 +0800

    commit

--- Classes/MSFApplyInfoCell.m
+++ Classes/MSFApplyInfoCell.m
@@ -10,14 +10,107 @@
 
 @implementation MSFApplyInfoCell
 
-- (void)awakeFromNib {
-    // Initialization code
+- (instancetype)initWithFrame:(CGRect)frame {
+	self = [super initWithFrame:frame];
+	if (self) {
+		self.backgroundColor = [UIColor whiteColor];
+		self.selectionStyle  = UITableViewCellSelectionStyleNone;
+		self.type = MSFCellTypeUnknown;
+	}
+	return self;
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
+- (void)setType:(MSFApplyInfoCellType)type {
+	if (type == _type) {
+		return;
+	}
+	_type = type;
+	if (type == MSFCellTypeUnknown) {
+		return;
+	}
+	[self.contentView.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+		[obj removeFromSuperview];
+	}];
+	switch (type) {
+		case MSFCellTypeContent:
+			[self contentCell];
+			break;
+		case MSFCellTypeSelection:
+			[self selectionCell];
+			break;
+		case MSFCellTypeCodePhone:
+			[self codePhoneCell];
+			break;
+		case MSFCellTypeCodePhoneExt:
+			[self codePhoneExtCell];
+			break;
+		case MSFCellTypePhoneSelection:
+			[self phoneSelectionCell];
+			break;
+		case MSFCellTypeSwitch:
+			[self switchCell];
+			break;
+		case MSFCellTypeAddContact:
+			[self addContactCell];
+			break;
+		case MSFCellTypeConfirm:
+			[self confirmCell];
+			break;
+		case MSFCellTypeBoldTitle:
+			[self boldTitleCell];
+			break;
+		case MSFCellTypeContentUnit:
+			[self contentUnitCell];
+			break;
+		case MSFCellTypeEduLength:
+			[self eduLengthCell];
+			break;
+		default: break;
+	}
+}
 
-    // Configure the view for the selected state
+- (void)contentCell {
+	
+}
+
+- (void)selectionCell {
+	
+}
+
+- (void)codePhoneCell {
+	
+}
+
+- (void)codePhoneExtCell {
+	
+}
+
+- (void)phoneSelectionCell {
+	
+}
+
+- (void)switchCell {
+	
+}
+
+- (void)addContactCell {
+	
+}
+
+- (void)confirmCell {
+	
+}
+
+- (void)boldTitleCell {
+	
+}
+
+- (void)contentUnitCell {
+	
+}
+
+- (void)eduLengthCell {
+	
 }
 
 @end
