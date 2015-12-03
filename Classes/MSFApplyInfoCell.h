c1904d78ef120fa0bc0d2ca1b3f43cb70b1aeade
zhaoyong <zhaoyo31@icloud.com>
2015-12-03 17:11:14 +0800

    commit

--- Classes/MSFApplyInfoCell.h
+++ Classes/MSFApplyInfoCell.h
@@ -9,19 +9,22 @@
 #import <UIKit/UIKit.h>
 
 typedef NS_ENUM(NSInteger, MSFApplyInfoCellType) {
-	MSFCellTypeContent				= 0,//输入框
-	MSFCellTypeSelection			= 1,//选择
-	MSFCellTypeCodePhone			= 2,//区号座机号
-	MSFCellTypeCodePhoneExt		= 3,//区号座机号分机号
-	MSFCellTypePhoneSelection = 4,//手机号电话本选择
-	MSFCellTypeSwitch					= 5,//开关
-	MSFCellTypeAddContact			= 6,//添加联系人
-	MSFCellTypeConfirm				= 7,//确认
-	MSFCellTypeBoldTitle			= 8,//粗体标题
-	MSFCellTypeContentUnit		= 9,//输入框收入单位
-	MSFCellTypeEduLength			= 10//学制
+	MSFCellTypeUnknown				= 0,//未知
+	MSFCellTypeContent				= 1,//输入框
+	MSFCellTypeSelection			= 2,//选择
+	MSFCellTypeCodePhone			= 3,//区号座机号
+	MSFCellTypeCodePhoneExt		= 4,//区号座机号分机号
+	MSFCellTypePhoneSelection = 5,//手机号电话本选择
+	MSFCellTypeSwitch					= 6,//开关
+	MSFCellTypeAddContact			= 7,//添加联系人
+	MSFCellTypeConfirm				= 8,//确认
+	MSFCellTypeBoldTitle			= 9,//粗体标题
+	MSFCellTypeContentUnit		= 10,//输入框收入单位
+	MSFCellTypeEduLength			= 11//学制
 };
 
 @interface MSFApplyInfoCell : UITableViewCell
+
+@property (nonatomic, assign) MSFApplyInfoCellType type;
 
 @end
