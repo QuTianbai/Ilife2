18f2fe4f2b8d3d27f2921b54f97133656918d638
zhaoyong <zhaoyo31@icloud.com>
2015-12-03 15:24:01 +0800

    新增 MSFApplyInfoCell、MSFApplyInfoTableViewController

---
+++ Classes/MSFApplyInfoCell.m
@@ -0,0 +1,23 @@
+//
+//  MSFApplyInfoCell.m
+//  Finance
+//
+//  Created by 赵勇 on 12/3/15.
+//  Copyright © 2015 MSFINANCE. All rights reserved.
+//
+
+#import "MSFApplyInfoCell.h"
+
+@implementation MSFApplyInfoCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
