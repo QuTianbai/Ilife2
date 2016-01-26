09e4a2560c6fe61656be9cfc229ba37d929e220f
Zēng Liàng <objczl@gmail.com>
2016-01-26 12:25:21 +0800

    Remove test code

--- Classes/MSFClient+Coupons.m
+++ Classes/MSFClient+Coupons.m
@@ -11,26 +11,6 @@
 @implementation MSFClient (Coupons)
 
 - (RACSignal *)fetchCouponsWithStatus:(NSString *)status {
-//TODO: 测试数据
-NSDictionary *representation = @{
-	@"id" : @"foo",
-	@"ticketRuleId" : @1,
-	@"ticketName" : @"bar",
-	@"type" : @"A",
-	@"phone" : @"1869699565",
-	@"receiveTime" : @"2015-05-03T15:38:45Z",
-	@"receiveChannel" : @"ios",
-	@"effectDateBegin" : @"2015-05-03T15:38:45Z",
-	@"effectDateEnd" : @"2015-08-03T15:38:45Z",
-	@"status" : @"1"
-};
-return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
-	MSFCoupon *coupon = [MTLJSONAdapter modelOfClass:[MSFCoupon class] fromJSONDictionary:representation error:nil];
-	[subscriber sendNext:coupon];
-	[subscriber sendNext:coupon];
-	[subscriber sendCompleted];
-	return nil;
-}];
 	NSURLRequest *request = [self requestWithMethod:@"GET" path:@"coupon/couponsList" parameters:@{
 		@"status": status,
 	}];
