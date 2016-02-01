18d3ece37803c75f1a43d1561b3289d1cd3ecb93
Zēng Liàng <objczl@gmail.com>
2016-02-01 17:46:26 +0800

    Fix coupons cell crash

--- Classes/MSFCouponTableViewCell.m
+++ Classes/MSFCouponTableViewCell.m
@@ -8,32 +8,39 @@
 #import <ReactiveCocoa/ReactiveCocoa.h>
 #import "MSFCouponViewModel.h"
 
+@interface MSFCouponTableViewCell ()
+
+@property (nonatomic, strong) MSFCouponViewModel *viewModel;
+
+@end
+
 @implementation MSFCouponTableViewCell
 
 - (void)awakeFromNib {
 	self.timeLeftLabel.transform = CGAffineTransformMakeRotation(45 * M_PI / 180);
+	RAC(self, titleLabel.text) = RACObserve(self, viewModel.title);
+	RAC(self, subtitleLabel.text) = RACObserve(self, viewModel.subtitle);
+	RAC(self, valueLabel.text) = RACObserve(self, viewModel.value);
+	RAC(self, introLabel.text) = RACObserve(self, viewModel.intro);
+	RAC(self, timeRangeLabel.text) = RACObserve(self, viewModel.timeRange);
+	RAC(self, timeLeftLabel.text) = RACObserve(self, viewModel.timeLeft);
+	
+	RAC(self, statusView.image) = [RACObserve(self, viewModel.imageName) map:^id(id value) {
+		return [UIImage imageNamed:value];
+	}];
+	RAC(self, deadlineView.image) = [RACObserve(self, viewModel.deadlineImageName) map:^id(id value) {
+		return [UIImage imageNamed:value];
+	}];
+	RAC(self, deadlineView.hidden) = [RACObserve(self, viewModel.isWarning) map:^id(id value) {
+		return @(![value boolValue]);
+	}];
+	RAC(self, timeLeftLabel.hidden) = [RACObserve(self, viewModel.isWarning) map:^id(id value) {
+		return @(![value boolValue]);
+	}];
 }
 
 - (void)bindViewModel:(MSFCouponViewModel *)viewModel {
-	RAC(self, titleLabel.text) = RACObserve(viewModel, title);
-	RAC(self, subtitleLabel.text) = RACObserve(viewModel, subtitle);
-	RAC(self, valueLabel.text) = RACObserve(viewModel, value);
-	RAC(self, introLabel.text) = RACObserve(viewModel, intro);
-	RAC(self, timeRangeLabel.text) = RACObserve(viewModel, timeRange);
-	RAC(self, timeLeftLabel.text) = RACObserve(viewModel, timeLeft);
-	
-	RAC(self, statusView.image) = [RACObserve(viewModel, imageName) map:^id(id value) {
-		return [UIImage imageNamed:value];
-	}];
-	RAC(self, deadlineView.image) = [RACObserve(viewModel, deadlineImageName) map:^id(id value) {
-		return [UIImage imageNamed:value];
-	}];
-	RAC(self, deadlineView.hidden) = [RACObserve(viewModel, isWarning) map:^id(id value) {
-		return @(![value boolValue]);
-	}];
-	RAC(self, timeLeftLabel.hidden) = [RACObserve(viewModel, isWarning) map:^id(id value) {
-		return @(![value boolValue]);
-	}];
+	self.viewModel = viewModel;
 }
 
 @end
