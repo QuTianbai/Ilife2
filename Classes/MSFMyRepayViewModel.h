643a0c392f2b60246a4032e09332abf60079aaea
xbm <baimiao.xu@msfinance.cn>
2016-02-27 12:40:27 +0800

    我的还款

---
+++ Classes/MSFMyRepayViewModel.h
@@ -0,0 +1,39 @@
+//
+//  MSFMyRepayViewModel.h
+//  Finance
+//
+//  Created by xbm on 16/2/27.
+//  Copyright © 2016年 MSFINANCE. All rights reserved.
+//
+
+#import <ReactiveViewModel/ReactiveViewModel.h>
+#import "MSFViewModelServices.h"
+
+@class MSFRepaymentSchedules;
+@class RACCommand;
+
+@interface MSFMyRepayViewModel : RVMViewModel
+
+@property (nonatomic, weak) id <MSFViewModelServices> services;
+
+@property (nonatomic, readonly) MSFRepaymentSchedules *model;
+
+// 合同商品title
+@property (nonatomic, readonly) NSString *contractTitle;
+//当前期数
+@property (nonatomic, readonly) NSString *loanCurrTerm;
+//贷款期数
+@property (nonatomic, readonly) NSString *loanTerm;
+//贷款类型
+@property (nonatomic, readonly) NSString *applyType;
+// 还款日
+@property (nonatomic, readonly) NSString *repayTime;
+// 本期应还款金额
+@property (nonatomic, readonly) NSString *repayMoney;
+
+- (instancetype)initWithModel:(id)model services:(id <MSFViewModelServices>)services;;
+
+- (RACSignal *)fetchPlanPerodicTablesSignal;
+
+
+@end
---
+++ Classes/MSFMyRepayViewModel.m
@@ -0,0 +1,62 @@
+//
+//  MSFMyRepayViewModel.m
+//  Finance
+//
+//  Created by xbm on 16/2/27.
+//  Copyright © 2016年 MSFINANCE. All rights reserved.
+//
+
+#import "MSFMyRepayViewModel.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import "MSFRepaymentSchedules.h"
+#import "NSDateFormatter+MSFFormattingAdditions.h"
+#import "msfclient+PlanPerodicTables.h"
+
+@implementation MSFMyRepayViewModel
+
+- (instancetype)initWithModel:(id)model services:(id <MSFViewModelServices>)services {
+	self = [super init];
+	if (!self) {
+		return nil;
+	}
+	_model = model;
+	_services = services;
+	RAC(self, loanCurrTerm) = RACObserve(self, model.loanCurrTerm);
+	RAC(self, loanTerm) = RACObserve(self, model.loanTerm);
+	//RAC(self, applyType) = RAC(<#TARGET, ...#>)
+	//	RAC(self, status) = [RACObserve(self, model.contractStatus) ignore:nil];
+	//	RAC(self, amount) = [RACObserve(self, model.repaymentTotalAmount) ignore:nil];
+	//	RAC(self, date) = [RACObserve(self, model.repaymentTime) map:^id(NSString *value) {
+	//		return value.length > 0 ? value : @"当天";
+	//	}];
+	//
+	//	RAC(self, ownerAllMoney) = [RACObserve(self, model.applmt) map:^id(id value) {
+	//		if (value == nil) {
+	//			return @"";
+	//		}
+	//		return value;
+	//	}];
+	//	RAC(self, contractLineDate) = [RACObserve(self, model.repaymentTime) map:^id(NSString *value) {
+	//		return value.length > 0 ? value : @"当天";
+	//	}];
+	
+	//	RAC(self, overdueMoney) = [RACObserve(self, model.rep) map:^id(id value) {
+	//		if (value == nil || [value isEqualToString:@"0.00"] ||[value isEqualToString:@"0"] || [value isEqualToString:@""]) {
+	//			return @"";
+	//		}
+	//		return [NSString stringWithFormat:@"已逾期:￥%@", value];
+	//	}];
+	//
+	//	RAC(self, cashAmount) = [RACObserve(self, model.cashDueMoney) ignore:nil];
+	//	RAC(self, cashDate) = [RACObserve(self, model.cashDueDate) map:^id(NSString *value) {
+	//		return value.length > 0 ? value : @"当天";
+	//	}];
+	
+	return self;
+}
+
+- (RACSignal *)fetchPlanPerodicTablesSignal {
+	return [self.services.httpClient fetchPlanPerodicTables:self.model];
+}
+
+@end
