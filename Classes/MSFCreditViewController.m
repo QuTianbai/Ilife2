fe76c0b558c112926f152e553314599f0e508404
Wyc <Wyc@Wyc.local>
2016-03-03 21:20:07 +0800

    改个错误

--- Classes/MSFCreditViewController.m
+++ Classes/MSFCreditViewController.m
@@ -6,7 +6,7 @@
 
 #import "MSFCreditViewController.h"
 //#import <Masonry.h>
-#import "MSFCreditCycleScrollView.h"
+
 //define this constant if you want to use Masonry without the 'mas_' prefix
 #define MAS_SHORTHAND
 //define this constant if you want to enable auto-boxing for default syntax
